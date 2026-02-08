67de133c776cf5baa7c049bbd05fb7fdda85534c
Xavier <xevans7@gatech.edu>
2026-02-07 23:39:37 -0500

    Added user/admin sign-in, updated base template to link to changes

---
+++ README.md
@@ -0,0 +1,9 @@
+Update this file with any extraneous info
+
+Current admin sign-in:
+Username - admin
+Password - adminpassword
+
+Standard User sign-i:
+Username - testuser
+Password - testuserpassword
--- posting/views.py
+++ posting/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+def index(request):
+    template_data = {}
+    template_data['title'] = 'Job Postings'
+    return render(request, 'home/posting.html', {'template_data': template_data})
+# Create your views here.
 
 # Create your views here.
