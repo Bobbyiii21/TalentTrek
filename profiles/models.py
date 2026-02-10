08a38c5f300ff90bf1a7235e999a1713db51e965
Ellie Fiftal <gfiftal@gmail.com>
2026-02-10 17:58:46 -0500

    Update models.py

--- profiles/models.py
+++ profiles/models.py
@@ -14,17 +14,17 @@ class DegreeType(models.TextChoices):
     BACHELORS = "BACHELORS", "Bachelor's"
     MASTERS = "MASTERS", "Master's"
     DOCTORATE = "DOCTORATE", "Doctorate"
+
 class Education(models.Model):
     grad_year = models.PositiveIntegerField(length=4) #if current student, they should put in projected grad date
     school_name = models.CharField(max_length=63)
-        degree = models.CharField(choices=DegreeType.choices, max_length=15)
-    degree = models.CharField(choices=degree_type, max_length=15)
+    degree = models.CharField(choices=DegreeType.choices, max_length=15)
+    links = models.TextField(max_length=127, help_text="Please enter links as Comma Separated Values") #check if list implemented propery; implement as a list of links that the job seeker can input to relevant sites such as a personal site or linkedin, etc
 
 #builds a summary of a job experience
 class Experience(models.Model):
     start_date = models.DateField()
-    end_date = models.DateField(default=date.today) #use html/css to hide this if currently employed
-    #sets default to today if that is the case (this will force the user to put a date if they are not currently employed)
+    end_date = models.DateField(blank=True) #use html/css to hide this if currently employed
     #Make sure to add a comment to user that date will not be displayed if they are currently employed.
     current_employee = models.BooleanField(default=False)
     company_name = models.CharField(max_length=31)
@@ -37,28 +37,25 @@ class Experience(models.Model):
 class User(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=63)
-    email = models.CharField(max_lenth=127)
+    email = models.CharField(max_length=127)
 
 #children of User model for different account types below
 
 #model for a job seeker
-#TODO: verify if lists of objects are implemented correctly and fix issues if not
 #TODO: add list of choices for skills somewhere (not in this app since they need to be used for search)
 #TODO: verify if country and city works properly
 #TODO: use html/css to make "links" into hyperlinks
 class JobSeeker(User):
     headline = models.TextField(max_length=1023)
-    education = models.ForeignKey([Education], on_delete=models.CASCADE) #list of education objects; this might need to be changed later, idrk how i should be implementing it as a list
+    education = models.ManyToManyField(Education) #list of education objects
     #skills = models.CharField(blank=True, choices=CHOICES, max_length=31) MAKE A LIST OF POSSIBLE SKILLS SOMEWHERE AND REPLACE "CHOICES" WITH APPROPRIATE VARIABLE
-    experience = models.ForeignKey([Experience], on_delete=models.CASCADE) #list of job experience objects; might need to be changed with education
-    links = models.TextField(max_length=127, help_text="Please enter links as Comma Separated Values") #check if list implemented propery; implement as a list of links that the job seeker can input to relevant sites such as a personal site or linkedin, etc
+    experience = models.ManyToManyField(Experience) #job experience objects
     country = models.ForeignKey(Country, on_delete=models.CASCADE, blank=True) #UNTESTED
     city = models.ForeignKey(City, on_delete=models.CASCADE, blank=True) #UNTESTED
 
 #model for a recruiter
 #TODO: build the model? if it needs anything
 class Recruiter(User):
-    #tbh idk if this needs any unique variables, just unique permissions, so im not sure if i need to actually put anything here since its essential properties are in its parent
-    pass
+    company = models.TextField(max_length=63)
 
 
