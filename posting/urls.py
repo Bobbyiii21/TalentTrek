fb60ef197c60a7e2be9d515a6041d949272e80a0
LukeFerg <fergusonluke21@gmail.com>
2026-02-09 23:20:59 -0500

    base.html edits and new links crearted

--- posting/urls.py
+++ posting/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='posting.index'),
-    #path('about', views.about, name='home.about'),
+    path('create', views.create, name='posting.create'),
+    path('explore', views.explore, name='posting.explore'),
 ]
--- posting/views.py
+++ posting/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
+
 def index(request):
     template_data = {}
     template_data['title'] = 'Job Postings'
-    return render(request, 'home/posting.html', {'template_data': template_data})
-# Create your views here.
+    return render(request, 'posting/index.html', {'template_data': template_data})
 
-# Create your views here.
+def create(request):
+    template_data = {}
+    template_data['title'] = 'Create a Job Posting'
+    return render(request, 'posting/create.html', {'template_data': template_data})
+
+def explore(request):
+    template_data = {}
+    template_data['title'] = 'Explore Job Postings'
+    return render(request, 'posting/explore.html', {'template_data': template_data})
