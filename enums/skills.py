aedb0f7f7b539a2e78d783003eb510fdae99a8f5
Bobby R. Stephens III <60822197+Bobbyiii21@users.noreply.github.com>
2026-02-12 16:21:19 -0500

    feat: skills enum, app  and model for cross-app usage

---
+++ enums/skills.py
@@ -0,0 +1,132 @@
+from enum import Enum
+
+class Skills(Enum):
+    # --------------------
+    # Programming languages
+    # --------------------
+    PYTHON = 'Python'
+    JAVA = 'Java'
+    JAVASCRIPT = 'JavaScript'
+    TYPESCRIPT = 'TypeScript'
+    RUBY = 'Ruby'
+    PHP = 'PHP'
+    CSHARP = 'C#'
+    GO = 'Go'
+    RUST = 'Rust'
+    SWIFT = 'Swift'
+    KOTLIN = 'Kotlin'
+    SCALA = 'Scala'
+    PERL = 'Perl'
+    LUA = 'Lua'
+
+    # --------------------
+    # Front‑end
+    # --------------------
+    HTML = 'HTML'
+    CSS = 'CSS'
+    REACT = 'React'
+    VUEJS = 'Vue.js'
+    ANGULAR = 'Angular'
+    SVELTE = 'Svelte'
+    NEXTJS = 'Next.js'
+    GATSBY = 'Gatsby'
+    REDUX = 'Redux'
+    JQUERY = 'jQuery'
+
+    # --------------------
+    # Back‑end / Server
+    # --------------------
+    NODEJS = 'Node.js'
+    EXPRESS = 'Express'
+    DJANGO = 'Django'
+    FLASK = 'Flask'
+    SPRING_BOOT = 'Spring Boot'
+    ASPNETCORE = 'ASP.NET Core'
+    RUBY_ON_RAILS = 'Ruby on Rails'
+    SYMFONY = 'Symfony'
+    LARAVEL = 'Laravel'
+    GRAPHQL = 'GraphQL'
+
+    # --------------------
+    # Databases
+    # --------------------
+    SQL = 'SQL'
+    MYSQL = 'MySQL'
+    POSTGRESQL = 'PostgreSQL'
+    MONGODB = 'MongoDB'
+    REDIS = 'Redis'
+    CASSANDRA = 'Cassandra'
+    SQLITE = 'SQLite'
+    ORACLE = 'Oracle'
+    FIREBASE = 'Firebase'
+
+    # --------------------
+    # DevOps / Cloud
+    # --------------------
+    AWS = 'AWS'
+    AZURE = 'Azure'
+    GCP = 'GCP'
+    DOCKER = 'Docker'
+    KUBERNETES = 'Kubernetes'
+    TERRAFORM = 'Terraform'
+    JENKINS = 'Jenkins'
+    GITLABCI = 'GitLab CI'
+    CICD = 'CI/CD'
+    ANSIBLE = 'Ansible'
+    VAGRANT = 'Vagrant'
+
+    # --------------------
+    # Data / ML
+    # --------------------
+    PYTORCH = 'PyTorch'
+    TENSORFLOW = 'TensorFlow'
+    SCIKIT_LEARN = 'scikit-learn'
+    NUMPY = 'NumPy'
+    PANDAS = 'pandas'
+    MATPLOTLIB = 'Matplotlib'
+    SEABORN = 'Seaborn'
+    SQLALCHEMY = 'SQLAlchemy'
+    KAFKA = 'Kafka'
+
+    # --------------------
+    # Agile / Methodology
+    # --------------------
+    SCRUM = 'Scrum'
+    AGILE = 'Agile'
+    SAFe = 'SAFe'
+    Kanban = 'Kanban'
+    XP = 'XP'          # Extreme Programming
+    TDD = 'TDD'        # Test‑Driven Development
+
+    # --------------------
+    # Design / Creative
+    # --------------------
+    CANVA = 'Canva'
+    PHOTOSHOP = 'Photoshop'
+    ILLUSTRATOR = 'Illustrator'
+    INDESIGN = 'InDesign'
+    PREMIERE_PRO = 'Premiere Pro'
+    AFTER_EFFECTS = 'After Effects'
+    AUDITION = 'Audition'
+    LIGHTROOM = 'Lightroom'
+    XD = 'Adobe XD'
+    FIGMA = 'Figma'
+    Balsamiq = 'Balsamiq'
+
+    # --------------------
+    # Testing / QA
+    # --------------------
+    SELENIUM = 'Selenium'
+    APPIUM = 'Appium'
+    JEST = 'Jest'
+    PYTEST = 'pytest'
+    CUCUMBER = 'Cucumber'
+    POSTMAN = 'Postman'
+
+    # --------------------
+    # Miscellaneous
+    # --------------------
+    REST_API = 'REST API'
+    SOAP = 'SOAP'
+    MQTT = 'MQTT'
+    WEB_SOCKETS = 'WebSockets'
