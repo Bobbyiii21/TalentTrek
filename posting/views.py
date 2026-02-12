c50db554d6eb4dcab450583671e5284bc87452d0
LukeFerg <fergusonluke21@gmail.com>
2026-02-12 18:02:20 -0500

    More base.html and style.css changes, new navbar for logged-in users

--- posting/views.py
+++ posting/views.py
@@ -1,15 +1,19 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
+@login_required
 def index(request):
     template_data = {}
     template_data['title'] = 'Job Postings'
     return render(request, 'posting/index.html', {'template_data': template_data})
 
+@login_required
 def create(request):
     template_data = {}
     template_data['title'] = 'Create a Job Posting'
     return render(request, 'posting/create.html', {'template_data': template_data})
 
+@login_required
 def explore(request):
     template_data = {}
     template_data['title'] = 'Explore Job Postings'
