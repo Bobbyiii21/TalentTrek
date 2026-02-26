1c66965603759bffd8806d9f83cc8caa0aa97bc0
Xavier <xevans7@gatech.edu>
2026-02-26 12:53:32 -0500

    Starting commit from main

--- profiles/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-# Create your views here.
-def index(request):
-    template_data = {}
-    template_data['title'] = 'Profiles'
-    return render(request, 'profiles/index.html', {'template_data': template_data})
-# Create your views here.
-
