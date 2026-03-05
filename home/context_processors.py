a81f5ba40fd046b39c71b502d57c86944606caa0
Xavier <xevans7@gatech.edu>
2026-03-05 13:07:31 -0500

    Bubble added for amount of unread notifications

--- home/context_processors.py
+++ home/context_processors.py
@@ -4,4 +4,5 @@ def base_data(request):
     base_data = {}
     if request.user.is_authenticated:
         base_data['base_data_notifications'] = Notification.objects.filter(recipient=request.user, read=False)
+        base_data['base_data_notifications_amount'] = len(base_data['base_data_notifications'])
     return base_data
