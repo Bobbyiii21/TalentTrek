e1f6a23dc9b08bea0c106bec2d2d23c3a6417a38
Bobby R. Stephens III <60822197+Bobbyiii21@users.noreply.github.com>
2026-03-09 00:55:41 -0400

    fix: url and modal fixes
    ### profiles.html
    
    - moved exp/edu modal outside respective loops
    - modified modals in order to preoccupy data on edit.
    - edited links in response to changes in views.py
    
    - modified js logic to preoccupy fields and
    
    - removed input validation on year fields (acceptible values in \<input> tags )
    
    - when editing the exp, the modal will properly hide the end date if the experience is current.
    
    ### views.py
    
    - func for normalization (enforcing https) and validation (making sure the text follows domain.TLD format with safe chars.)
    
    - incorporated functions into existing logic
    
    - various logic clean up

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,4 @@
 import csv
-import io
 import re
 from django.contrib.auth import login as auth_login, authenticate, logout as auth_logout
 from django.contrib.auth.decorators import login_required
@@ -13,12 +12,23 @@ from posting.models import Post
 from applications.models import Application
 
 
+def normalize_link(url: str) -> str:
+    """Ensure URL has a scheme (https://) for safe use in href."""
+    u = (url or '').strip()
+    if not u:
+        return u
+    if 'https://' not in u:
+        if 'http://' not in u:
+            return f'https://{u}'
+        else:
+            return u.replace('http://', 'https://')
+    return u
+
+
 def validate_urls(urls: str) -> list[str]:
     """Validate a string of comma-separated URLs. Returns an empty list if the URL is invalid."""
-
     return_urls = re.findall(r'(?:http[s]?:\/\/.)?(?:www\.)?[-a-zA-Z0-9@%._\+~#=]{2,256}\.[a-z]{2,6}\b(?:[-a-zA-Z0-9@:%_\+.~#?&\/\/=]*)', urls)
-    print(return_urls)
-    return return_urls
+    return [normalize_link(u) for u in return_urls]
     
 
 @login_required
@@ -76,7 +86,7 @@ def onboard(request):
             if ('resume' in request.FILES):
                 job_seeker.resume = request.FILES['resume']
             if (request.POST['links'] and request.POST['links'].strip()):
-                job_seeker.links = request.POST['links'].strip()
+                job_seeker.links = ''.join(f"{link}," for link in validate_urls(request.POST['links'].strip()))
             request.user.save()
             job_seeker.save()
         else:
@@ -90,7 +100,7 @@ def onboard(request):
             if (request.POST['company'] and request.POST['company'].strip()):
                 recruiter.company = request.POST['company'].strip()
             if (request.POST['links'] and request.POST['links'].strip()):
-                recruiter.links = request.POST['links'].strip()
+                recruiter.links = ''.join(f"{link}," for link in validate_urls(request.POST['links'].strip()))
             request.user.save()
             recruiter.save()
         return redirect('accounts.profiles', user_link=str(request.user))
@@ -115,18 +125,14 @@ def profiles(request, user_link):
         template_data['skills_options'] = Skill.objects.all().order_by('name')
         template_data['experience'] = seeker_user.experience.all()
         template_data['degree_choices'] = Education.DegreeType.choices
-        template_data['links'] = seeker_user.links.split(",")
-        template_data['links'].pop()
-        for link in template_data['links']:
-            link = link.strip()
+        t_links = [normalize_link(l.strip()) for l in seeker_user.links.split(",") if l.strip()]
+        template_data['links'] = t_links
         #find some way to put resume 
     elif template_data['is_recruiter']:
         recruiter_user = Recruiter.objects.get(user_id=id)
         template_data['recruiter_user'] = recruiter_user
-        template_data['links'] = recruiter_user.links.split(",")
-        template_data['links'].pop()
-        for link in template_data['links']:
-            link = link.strip()
+        t_links = [normalize_link(l.strip()) for l in recruiter_user.links.split(",") if l.strip()]
+        template_data['links'] = t_links
 
 
     if request.method == 'POST':
@@ -224,8 +230,7 @@ def profiles(request, user_link):
                 edit_experience.company_name = request.POST['company_name']
                 edit_experience.position_title = request.POST['position_title']
                 edit_experience.job_description = request.POST['job_description']
-                try: edit_experience.current_employee = bool(request.POST['current_employee'])
-                except: new_experience.current_employee = False
+                edit_experience.current_employee = bool(request.POST.get('current_employee', False))
                 edit_experience.start_date = request.POST['start_date']
                 if not edit_experience.current_employee:
                     edit_experience.end_date = request.POST['end_date']
@@ -243,17 +248,15 @@ def profiles(request, user_link):
 
             if request.POST['subfield'] == 'link_add':
                 links = validate_urls(request.POST['link'])
-                for link in links:
-                    seeker_user.links += f"{link},"
+                seeker_user.links += ''.join(f"{link}," for link in links)
                 seeker_user.save()
-                for link in links:
-                    template_data['links'].append(link)
+                template_data['links'].extend(links)
 
             if request.POST['subfield'] == 'link_delete':
-                seeker_user.links = seeker_user.links.replace(f"{request.POST['link'].strip()},", '')
+                to_remove = request.POST['link'].strip()
+                seeker_user.links = seeker_user.links.replace(f"{to_remove},", '')
                 seeker_user.save()
-                #Removes all copies of deleted link since template data stuff is done before in the code
-                template_data['links'][:] = [item for item in template_data['links'] if item != request.POST['link'].strip()]
+                template_data['links'][:] = [item for item in template_data['links'] if item != to_remove]
 
             if request.POST['subfield'] == 'skill_add':
                 for skill in request.POST['skills']:
@@ -277,16 +280,16 @@ def profiles(request, user_link):
 
         elif user.is_recruiter:
             if request.POST['subfield'] == 'link_add':
-                recruiter_user.links += f"{request.POST['link'].strip()},"
+                links = validate_urls(request.POST['link'])
+                recruiter_user.links += ''.join(f"{link}," for link in links)
                 recruiter_user.save()
-                template_data['links'].append(request.POST['link'].strip())
+                template_data['links'].extend(links)
 
             if request.POST['subfield'] == 'link_delete':
-                recruiter_user.links = recruiter_user.links.replace(f"{request.POST['link'].strip()},", '')
+                to_remove = request.POST['link'].strip()
+                recruiter_user.links = recruiter_user.links.replace(f"{to_remove},", '')
                 recruiter_user.save()
-                #Removes all copies of deleted link since template data stuff is done before in the code
-                template_data['links'][:] = [item for item in template_data['links'] if item != request.POST['link'].strip()]
-
+                template_data['links'][:] = [item for item in template_data['links'] if item != to_remove]
 
     return render(request, 'accounts/profiles.html', {'template_data': template_data})
 
